1429877697cc1d84e45354964f4bfae113c9e0f5
hanmango-o <hanmango.o@gmail.com>
2022-10-09 19:32:43 +0900

    fix : Modify Bluetooth logic, apply design pattern
    기존 블루투스 로직을 Deprecated하고 새로운 로직을 추가함
    Strategy Pattern을 사용하여 로직을 수정함
    
    v0.6.0 | 한영찬(hanmango-o) | hanmango.o@gmail.com

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:safelight/ui/view/sign_in_view.dart';
 import 'package:safelight/view_model/controller/blue_controller.dart';
 import 'package:safelight/view_model/controller/user_controller.dart';
 
-// v0.5.1 | 한영찬(hanmango-o) | hanmango.o@gmail.com
+// v0.6.0 | 한영찬(hanmango-o) | hanmango.o@gmail.com
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
