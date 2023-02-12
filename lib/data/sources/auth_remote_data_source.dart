a953ab76c8c77f26d8718f4593ff713e73f25b40
hanmango-o <hanmango.o@gmail.com>
2023-02-12 21:10:07 +0900

    docs : add docs in datasource

--- lib/data/sources/auth_remote_data_source.dart
+++ lib/data/sources/auth_remote_data_source.dart
@@ -2,6 +2,18 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import '../../core/errors/exceptions.dart';
 
+/// [AuthRemoteDataSource]는 사용자 인증(로그인/로그아웃)과 관련된 기능을 수행한다.
+///
+/// 모든 사용자 인증은 [FirebaseAuth]를 사용하며,
+///
+/// ---
+/// ## Functions
+/// [AuthRemoteDataSource]는 아래의 메소드를 호출하여 기능을 수행할 수 있다.
+///
+/// |기능||설명|
+/// |:-------|-|:--------|
+/// |[signInAnonymously]||FirebaseAuth를 통한 익명 로그인|
+/// |[signOutAnonymously]||FirebaseAuth를 통한 익명 로그아웃|
 abstract class AuthRemoteDataSource {
   Future<void> signInAnonymously();
   Future<void> signOutAnonymously();
